acc0cf41d589fe3bd5a3bae3aafe93df40609533
sanaantoun <103603707+sanaantoun@users.noreply.github.com>
2022-05-21 11:42:49 -0400

    Delete moredescription.js

--- moredescription.js
+++
@@ -1,6 +0,0 @@
-const readMoreBtn = document.querySelector('.moredescbutton');
-const text = document.querySelector('.moredesctext');
-
-readMoreBtn.addEventListener('click', (e)=>{
-    text.classList.toggle('show-more');
-})
